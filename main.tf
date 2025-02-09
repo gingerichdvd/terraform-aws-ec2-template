3460918c8445892bdd52315703eaf01347f3d4a5
Bird <bird@localhost.localdomain>
2025-02-09 14:19:16 +0900

    fix: Add comments

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Creates a Virtual Private Cloud to host ec2 instances
 resource "aws_vpc" "mtc_vpc" {
   cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
@@ -8,6 +9,7 @@ resource "aws_vpc" "mtc_vpc" {
   }
 }
 
+# Defines subnet of private ip addresses for the VPC
 resource "aws_subnet" "mtc_public_subnet" {
   vpc_id                  = aws_vpc.mtc_vpc.id
   cidr_block              = "10.10.0.0/24"
@@ -19,6 +21,7 @@ resource "aws_subnet" "mtc_public_subnet" {
   }
 }
 
+# Creates a gateway for traffic to flow in and out of the VPC to the internet
 resource "aws_internet_gateway" "mtc_internet_gateway" {
   vpc_id = aws_vpc.mtc_vpc.id
 
@@ -27,6 +30,7 @@ resource "aws_internet_gateway" "mtc_internet_gateway" {
   }
 }
 
+# Creates a routing table for the VPC
 resource "aws_route_table" "mtc_public_rt" {
   vpc_id = aws_vpc.mtc_vpc.id
 
@@ -35,23 +39,27 @@ resource "aws_route_table" "mtc_public_rt" {
   }
 }
 
+# Defines a defalut route for instances inside the VPC to send traffic to the internet
 resource "aws_route" "default_route" {
   route_table_id         = aws_route_table.mtc_public_rt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
 }
 
+# Defines associations in the routing table
 resource "aws_route_table_association" "mtc_route_assoc" {
   subnet_id      = aws_subnet.mtc_public_subnet.id
   route_table_id = aws_route_table.mtc_public_rt.id
 }
 
+# Defines ACL allowing what traffic enters and leaves the VPC
 resource "aws_security_group" "mtc_sg" {
   #Security group has a name attribute, doesn't need to be tagged
   name        = "dev-sg"
   description = "dev security group"
   vpc_id      = aws_vpc.mtc_vpc.id
 
+  # Allows all traffic from a given ip address, blocks trafffic from all other ip addresses
   ingress {
     from_port = 0
     to_port   = 0
@@ -60,10 +68,22 @@ resource "aws_security_group" "mtc_sg" {
     cidr_blocks = [var.private_ip]
   }
 
+  # Allows all traffic to leave the VPC and access the internet
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Defines EC2 instance that will be run inside the VPC
+resource "aws_instance" "app_server" {
+  # AMI for Red Hat linux server
+  ami           = "ami-0da4b082c0455e0a0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
+# Gives the instance id of a created EC2
 output "instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.app_server.id
 }
 
+# Gives the public ip address of the EC2
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server.public_ip
